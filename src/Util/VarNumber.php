0a584e1bf999f92d241220263fe5ce47fbb071c8
Murfidar <murfidar@gmail.com>
2024-02-29 10:01:43 +0700

    chore: change to VarNumber

---
+++ src/Util/VarNumber.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace BRI\Util;
+
+use Exception;
+
+final class VarNumber
+{
+  // generate random number
+  public function generateVar(int $randomLength): int
+  {
+    if ($randomLength < 1) {
+      throw new Exception('Numbers length must be greater than 0');
+    }
+    $min = pow(10, $randomLength - 1);
+    $max = pow(10, $randomLength) - 1;
+    $var = rand($min, $max);
+    return $var;
+  }
+}
